012cccf93a946e50add333218c83484116bce270
sandhya-ch <sandhyareddych1997@gmail.com>
2020-07-12 13:47:10 +0530

    notes list implemented

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Note
 
 # Create your views here.
+def notes_list(request):
+    notes = Note.objects.all()
+    return render(request, 'blog/notes_list.html', {'notes':notes})
