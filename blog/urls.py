5cffb38a191bae4c7466fd3008a6e4b5629cdc06
sandhya-ch <sandhyareddych1997@gmail.com>
2020-07-12 16:52:32 +0530

    notes update, notes detail and note deletes are implemented

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.notes_list, name='notes_list')
+    path('',views.notes_list, name='notes_list'),
+    path('note/<int:pk>/',views.note_detail, name='note_detail'),
+    path('note/new/',views.note_new, name='note_new'),
+    path('note/edit/<int:pk>/',views.note_edit, name='note_edit'),
+    path('note/remove/<int:pk>',views.note_remove, name='note_remove')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect
 from .models import Note
+from .forms import NoteForm
 
 # Create your views here.
 def notes_list(request):
-    notes = Note.objects.all()
+    notes = Note.objects.filter(user=request.user)
     return render(request, 'blog/notes_list.html', {'notes':notes})
+
+def note_detail(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    return render(request, 'blog/note_detail.html', {'note':note})
+    
+def note_new(request):
+    if request.method=='POST':
+        form=NoteForm(request.POST)
+        if form.is_valid():
+            note=form.save(commit=False)
+            note.user=request.user
+            note.save()
+            return redirect('note_detail', pk=note.pk)
+    else:
+        form=NoteForm()
+    return render(request, 'blog/note_new.html', {'form':form})
+   
+def note_edit(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    if request.method=='POST':
+        form=NoteForm(request.POST,instance=note)
+        if form.is_valid():
+            note=form.save()
+            return redirect('note_detail', pk=note.pk)
+    else:
+        form=NoteForm(instance=note)
+    return render(request, 'blog/note_new.html', {'form':form})
+
+def note_remove(request, pk):
+    post = get_object_or_404(Note, pk=pk)
+    post.delete()
+    return redirect('notes_list')
